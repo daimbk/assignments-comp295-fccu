a3fcaa03e91eb8fcda6f6ba4d2c88c7d6360b929
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-03-23 11:22:23 +0500

    tiles initialized and terminal clearing added

--- ass1.cpp
+++ ass1.cpp
@@ -3,6 +3,7 @@ Name: Daim Bin Khalid
 Roll no: 251686775*/
 
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -11,6 +12,39 @@ using namespace std;
 int (*game_grid())[4]
 {
     static int grid[4][4] = {0};
+
+    // generate 2 random tiles with 2 or 4
+    // set seed
+    srand(time(0));
+
+    int tile_score = rand() % 2;
+    int i = rand() % 4;
+    int j = rand() % 4;
+    if (tile_score == 0)
+    {
+        tile_score = 2;
+        grid[i][j] = tile_score;
+    }
+    else
+    {
+        tile_score = 4;
+        grid[i][j] = tile_score;
+    }
+
+    int second_tile_score = rand() % 2;
+    i = rand() % 4;
+    j = rand() % 4;
+    if (second_tile_score == 0)
+    {
+        second_tile_score = 2;
+        grid[i][j] = second_tile_score;
+    }
+    else
+    {
+        second_tile_score = 4;
+        grid[i][j] = second_tile_score;
+    }
+
     return grid;
 }
 
@@ -31,5 +65,14 @@ void display_grid()
 
 int main()
 {
-    display_grid();
+    string user_input;
+    while (user_input != "exit")
+    {
+        system("cls");
+        display_grid();
+        cout << "Press any key: ";
+        cin >> user_input;
+    }
+
+    return 0;
 }
