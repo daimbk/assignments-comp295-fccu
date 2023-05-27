5519f0cb17c2bdc86417b0dd0d85b81dbf70471e
Daim Bin Khalid <51926730+daimbk@users.noreply.github.com>
2023-05-27 13:16:02 +0500

    Task1 done: IntComplex files

---
+++ ass2/IntComplex.h
@@ -0,0 +1,21 @@
+#ifndef INTCOMPLEX_H // wrapper to handle multiple header import errors
+#define INTCOMPLEX_H
+
+class IntComplex
+{
+private:
+    int real;
+    int imaginary;
+
+public:
+    // declare constructor
+    IntComplex(int real_input, int imaginary_input);
+
+    // declare overload arithmetic operators
+    IntComplex operator+(const IntComplex &object);
+    IntComplex operator-(const IntComplex &object);
+    IntComplex operator*(const IntComplex &object);
+    IntComplex operator/(const IntComplex &object);
+};
+
+#endif
